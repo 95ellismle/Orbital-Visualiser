d12f8f9ac17609d28a04c9e493c5aafa8703b7a8
Matt <95ellismle@gmail.com>
2018-11-17 18:18:34 +0000

    Cleaned commit and added more to the gitignore

--- Templates/permanent_settings.py
+++ Templates/permanent_settings.py
@@ -1,5 +1,5 @@
 previous_path = '/home/Sangeya/copy/'
-previous_runtime = '17/11/18 44:17:24'
+previous_runtime = '17/11/18 16:18:00'
 tachyon_path = '/usr/local/lib/vmd/tachyon_LINUXAMD64'
 time_format = "%d/%m/%y %M:%H:%S"
 created_docs = True
