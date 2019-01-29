1378304f00a13bd04037fa35925266c43bfac407
Matt <95ellismle@gmail.com>
2019-01-29 18:01:40 +0000

    Added a little script to change the length of a movie, also almost finished the documentation creator code.

--- Create_docs.py
+++ Create_docs.py
@@ -9,79 +9,25 @@ tidying!
 """
 from collections import OrderedDict
 import os
+import re
 
 from src import IO as io
 from src import EXCEPT as EXC
 
-
-def replace_filepaths_in_template(template_text, replacers):
-    """
-    """
-    for replace in replacers:
-        template_text = template_text.replace(replace, replacers[replace])
-    return template_text
-
-
-
-
-
-section_header = 'h3'
-table_tag = "<table id=table1>"
-
-# Define folders
-docs_folder = io.folder_correct('./Docs')
-templates_folder = io.folder_correct('./Templates/HTML')
-static_folder = io.folder_correct(docs_folder+"Static")
-
-# Where to save the final HTML files
-html_filepaths = {'table': io.folder_correct(docs_folder + "tables/",
-                                             make_file=True),
-                  'index': io.folder_correct("./Documentation.html"),
-                  'how_it_works': io.folder_correct(static_folder +
-                                                    "How_it_works.html"),
-                  'FAQs': io.folder_correct(static_folder +
-                                            "FAQs.html"),
-                  'How_to_edit': io.folder_correct(static_folder +
-                                                   "How_to_edit.html"),
-                  'examples': io.folder_correct(static_folder +
-                                                "Examples.html")}
-
-# The filepaths to the files acting as the templates to the webpages
-template_filepaths = {}
-for f in os.listdir(templates_folder):
-    fName = f.replace('.html', '')
-    template_filepaths[fName] = templates_folder + f
-
-# A dictionary of things to find in the HTML files and replace
-replacers = {"*index_file*": html_filepaths['index'],
-             "*doc_img_folder*": io.folder_correct(docs_folder+"img"),
-             "*how_it_works*": html_filepaths['how_it_works'],
-             "*How_to_edit*": html_filepaths["How_to_edit"],
-             "*examples*": html_filepaths['examples'],
-             "*FAQs*": html_filepaths['FAQs'],
-             "*docs_folder*": docs_folder,
-             "*vendor_folder_path*": io.folder_correct(docs_folder+"vendor"),
-             "*css_folder_path*": io.folder_correct(docs_folder+"css"),
-             "*quick_start*": io.open_read(static_folder+"Quick_Start.html"),
-             "*intro_text*": io.open_read(static_folder+"Intro.html"),
-             "*header_text*": io.open_read(static_folder+"Header_text.html"),
-             "*top_nav*": io.open_read(static_folder+"TopNav.html"),
-             "*sidebar_text*": "",
-             "*title*": "Movie Maker Documentation",
-             }
-
-# The default settings filepath
-defaults_filepath = io.folder_correct('./Templates/') + 'defaults.py'
+import docstring as docstr
 
 
 class ParseDefaults(object):
     """
     Will parse the default settings file. Stores all the parsed setting in a
     dictionary called self.params. This is divided up by the section header in
-    the defaults dict file
+    the defaults dict file.
+
+    This class will also create the filepaths for the table HTML files
 
     Inputs:
         * default settings filepath (filepath to the defaults.py file)
+        * HTMLFiles = the dictionary storing the filepaths for the HTML files
 
     (No Public Functions)
     """
@@ -94,12 +40,21 @@ class ParseDefaults(object):
         defaultsTxt = self._getDefaultsDict()
         self._separateSections(defaultsTxt)  # Get sections in defaults dict
 
+        # Parse each line
+        remKeys = []
         for section in self.params:
-            for line in self.params[section]['ltxt']:
-                setting, params = self._parseLine(line)
-                if setting:
-                    self.params[section][setting] = params
-            self.params[section].pop('ltxt')
+            if section.strip():
+                for line in self.params[section]['ltxt']:
+                    setting, params = self._parseLine(line)
+                    if setting:
+                        self.params[section][setting] = params
+                self.params[section].pop('ltxt')
+            else:
+                remKeys.append(section)
+
+        # Remove redundant keys
+        for key in remKeys:
+            self.params.pop(key)
 
     def _getDefaultsDict(self):
         """
@@ -110,7 +65,7 @@ class ParseDefaults(object):
 
             words = [i.strip() for i in line.split(' ')]
             if all(word in words for word in ('defaults', '=', '{')):
-                return ltxt[linei:]
+                return ltxt[linei+1:]
         else:
             msg = "\nCouldn't find the declaration of the `defaults' variable!"
             msg += "\n\nMake sure there is a line in the defaults.py file with"
@@ -152,11 +107,11 @@ class ParseDefaults(object):
         the first word behind the ' : ' character.
         """
         # First get the setting
-        setting, rest = self._split_by(line,
-                                       ' : ',
-                                       2,
-                                       True,
-                                       2)
+        setting, rest = self.__split_by(line,
+                                        ' : ',
+                                        2,
+                                        True,
+                                        2)
         # Remove the string marks from the setting
         setting = setting.strip().strip("'").strip('"')
         return setting, rest
@@ -168,11 +123,11 @@ class ParseDefaults(object):
 
         line2 = the line without the setting in (use __getSetting first)
         """
-        value, rest = self._split_by(line2,
-                                     ', #',
-                                     2,
-                                     True,
-                                     2)
+        value, rest = self.__split_by(line2,
+                                      ', #',
+                                      2,
+                                      True,
+                                      2)
         return eval(value), rest
 
     def __getExplanationOptionsTested(self, line3):
@@ -180,11 +135,11 @@ class ParseDefaults(object):
         Will parse the explanations of the setting, the Options available to
         set it and whether it's been tested or not.
         """
-        tmp = self._split_by(line3,
-                             '|',
-                             3,
-                             False,
-                             1)
+        tmp = self.__split_by(line3,
+                              '|',
+                              3,
+                              False,
+                              1)
         if len(tmp) == 3:
             explan, options, tested = tmp
             tested = 'not' not in tested
@@ -198,7 +153,7 @@ class ParseDefaults(object):
             msg += line3
             raise SystemExit(msg)
 
-    def _split_by(self, line, by, length=2, congeal_last=False, min_lines=2):
+    def __split_by(self, line, by, length=2, congeal_last=False, min_lines=2):
         """
         Split a string ('line') by the 'by' variable.
 
@@ -239,137 +194,170 @@ class ParseDefaults(object):
         return split
 
 
-defaults = ParseDefaults(defaults_filepath)
+class SideBar(object):
+    """
+    Will add the parameters to the replacers dictionary (*var* = param[var]).
+    Will Create the sidebar text and the sidebar filepaths
 
-#for section in params:
-#    for param in params[section]:
-#        setting, rest_of_stuff = split_by(param, ' : ', 2)
-#        setting = setting.strip().replace("'", '')
-#        tmp = split_by(rest_of_stuff, ', #', 100, True)
-#        default_value, rest_of_stuff = tmp
-#        try:
-#            if type(eval(default_value)) == dict:
-#                default_value = eval(default_value)
-#                s = "%s\n" % "<table class=subtable>"
-#                s += """
-#                <tr>
-#                    <th> <font size="2.5"> Key </font> </th>
-#                    <th> <font size="2.5"> Value </font> </th>
-#                </tr>
-#                """
-#                for key in default_value:
-#                    s += """
-#                <tr>
-#                    <td> <font size="2"> %s </font> </td>
-#                    <td> <font size="2"> %s </font> </td>
-#                </tr>""" % (str(key), str(default_value[key]))
-#                default_value = s + "</table>"
-#        finally:
-#            pass
-#        if "DEPRECATED" not in rest_of_stuff:
-#            tmp = rest_of_stuff.replace('#', '').strip().split('|')
-#            description, types, tested = tmp
-#            types = eval(types)
-#            tested = ('not' not in tested)
-#            params_parsed[section][setting]['default'] = default_value
-#            params_parsed[section][setting]['tested'] = tested
-#            params_parsed[section][setting]['types'] = types
-#            params_parsed[section][setting]['desc'] = description
-#        else:
-#            params_parsed[section].pop(setting, None)
-#
-#all_variables_which_section = {}
-#for section in params_parsed:
-#    for param in params_parsed[section]:
-#        all_variables_which_section[param] = section
-#
-#table_filepaths = {i: io.folder_correct(html_filepaths['table'] +
-#                                        str(i) +
-#                                        ".html")
-#                   for i in params_parsed}
-#for section in params_parsed:
-#    replacers['*sidebar_text*'] += """
-#                <li>
-#                    <a href="%s"> %s </a>
-#                </li>""" % (table_filepaths[section], section)
-#
-#replacers['*header_text*'] = replace_filepaths_in_template(replacers['*header_text*'],
-#                                                           replacers)
-#replacers['*top_nav*'] = replace_filepaths_in_template(replacers['*top_nav*'],
-#                                                       replacers)
-#for key in template_data:
-#    template_data[key] = replace_filepaths_in_template(template_data[key],
-#                                                       replacers)
-#
-#for variable in all_variables_which_section:
-#    for section in template_data:
-#        link_txt = '''<a href="%s" class="param_link">
-#        <code  class="CODE"> %s </code>
-#        </a>''' % (table_filepaths[all_variables_which_section[variable]],
-#                   variable)
-#        template_data[section] = template_data[section].replace("*%s*" % variable,
-#                                                                link_txt)
-#
-## Write html files which aren't a table file
-#for i in html_filepaths:
-#    if 'tab' not in i:
-#        io.open_write(html_filepaths[i], template_data[i])
-#
-## Change bool defaults to yes or no
-#for section in params_parsed:
-#    for param in params_parsed[section]:
-#        if params_parsed[section][param]['default'].strip() == 'True':
-#            params_parsed[section][param]['default'] = "'yes'"
-#        elif params_parsed[section][param]['default'].strip() == 'False':
-#            params_parsed[section][param]['default'] = "'no'"
-#
-## Create and write the tables
-#for table_title in table_filepaths:
-#    table_file_text = template_data['table']
-#    table_file_text = table_file_text.replace("*table_name*", table_title)
-#    s = """%s""" % table_tag
-#    s += """<tr>
-#    <th>
-#      Setting
-#    </th>
-#    <th>
-#      Default Value
-#    </th>
-#    <th>
-#      Description
-#    </th>
-#    <th>
-#      Input Type
-#    </th>
-#  </tr>\n"""
-#    for param in params_parsed[table_title]:
-#        type_str = """
-#        <ul>"""
-#        for T in params_parsed[table_title][param]['types']:
-#            type_str += """
-#            <li>  %s </li>""" % str(T)
-#        type_str += """
-#        </ul>"""
-#        s += """<tr>
-#    <td>
-#      %s
-#    </td>
-#    <td>
-#      %s
-#    </td>
-#    <td>
-#      %s
-#    </td>
-#    <td>
-#      %s
-#    </td>
-#  </tr>\n""" % (param,
-#                params_parsed[table_title][param]['default'],
-#                params_parsed[table_title][param]['desc'],
-#                type_str)
-#
-#    s += "</table>\n\n<br>\n<br>\n\n"
-#    table_file_text = table_file_text.replace("*table_data*", s)
-#
-#    io.open_write(table_filepaths[table_title], table_file_text)
-#
+    Inputs:
+        * defaults = the ParseDefaults object that parses the default settings
+                      file
+        * replacers = replacer dictionary to add the parameters to
+    """
+    def __init__(self, defaults, replacers, tablesFolder):
+        self.params = defaults.params
+        self.replacers = replacers
+        self.tablesFolder = tablesFolder
+
+        self._createSidebar()
+        self._addParametersToReplacers()
+
+    def _addParametersToReplacers(self):
+        """
+        Will add the parameters to the replacers dictionary.
+        """
+        for section in self.params:
+            link = self.tableFilePaths[section]
+
+            for param in self.params[section]:
+                htmlLink = '<a href="%s"> %s </a>' % (link, param)
+                self.replacers['*%s*' % param] = htmlLink
+
+    def _createSidebar(self):
+        """
+        Create the sidebar text to use in the HTML files. Also creates the dict
+        of filepaths.
+        """
+        self.tableFilePaths = {}
+
+        sidebarTxt = ''
+        for sectionName in self.params:
+            tableFilePath = '%s%s.html' % (self.tablesFolder,
+                                           sectionName)
+            sidebarTxt += '<li>\n'
+            sidebarTxt += '%s<a href="%s"> %s </a>' % ("\t" * 5,
+                                                       tableFilePath,
+                                                       sectionName)
+            sidebarTxt += '\n</li>\n'
+            self.tableFilePaths[sectionName] = tableFilePath
+
+        self.replacers['*sidebar_text*'] = sidebarTxt
+
+
+class HTMLFile(object):
+    """
+    Will create a HTML file from the template file given. This will involve
+    replacing the *vars* using the replacer dictionary and fixing any links
+    using the html filepaths dictionary.
+
+    Inputs:
+        * templateFilePath = The path to the template file
+                               (e.g. in Templates/HTML/*.html)
+
+        * replacers = the dictionary which determines what the strings that
+                       follow the *var* patter are replaced with.
+
+        * htmlFiles = the dictionary containing the links to replace the
+                       relative links with
+    """
+    def __init__(self, templateFilePath, replacers, defaults):
+        self.filePath = templateFilePath
+        self.replacers = replacers
+        self.fileTxt = io.open_read(templateFilePath)
+        self.defaults = defaults
+        self.params = defaults.params
+
+        self._replaceVars()
+
+    def _replaceVars(self):
+        """
+        Will replace the variables in the file text (self.fileTxt) with the
+        relevant variable in replacers
+        """
+        allVars = re.findall(r'\*[a-zA-Z_]+.?\*', self.fileTxt)
+
+        # Error Checking
+        for var in allVars:
+            if var not in self.replacers:
+                msg = "Can't find the variable %s" % var
+                msg += " in the replacers dictionary.\n\n"
+                msg += "This is found in the file %s\n\n\n" % self.filePath
+                raise SystemExit(msg)
+
+        for var in allVars:
+            self.fileTxt = self.fileTxt.replace(var,
+                                                self.replacers[var])
+
+
+class TableHTMLFile(HTMLFile):
+    """
+    Special case HTML file creator for the table files. Will create the tables
+    in the table files.
+    """
+    def __init__(self, tableFilePath, sectionParams):
+        self.filePath = tableFilePath
+        self.fileTxt = io.open_read(self.filePath)
+        self.params = sectionParams
+
+
+section_header = 'h3'
+table_tag = "<table id=table1>"
+
+# Define folders
+docs_folder = io.folder_correct('./Docs')
+templates_folder = io.folder_correct('./Templates/HTML')
+static_folder = io.folder_correct(docs_folder+"Static")
+tables_folder = io.folder_correct(docs_folder+"Tables")
+
+
+# The filepaths to the files acting as the templates to the webpages
+template_filepaths = {}
+for f in os.listdir(templates_folder):
+    fName = f.replace('.html', '')
+    template_filepaths[fName] = templates_folder + f
+
+# The default settings filepath
+defaults_filepath = io.folder_correct('./Templates/') + 'defaults.py'
+
+defaults = ParseDefaults(defaults_filepath)
+# A dictionary of things to find in the HTML files and replace
+replacers = {"*doc_img_folder*": io.folder_correct(docs_folder+"img"),
+             "*docs_folder*": docs_folder,
+             "*vendor_folder_path*": io.folder_correct(docs_folder+"vendor"),
+             "*css_folder_path*": io.folder_correct(docs_folder+"css"),
+             "*quick_start*": io.open_read(templates_folder+"QuickStart.html"),
+             "*intro_text*": io.open_read(templates_folder+"Intro.html"),
+             "*header_text*": io.open_read(templates_folder+"HeaderTxt.html"),
+             "*Misc*": io.open_read(templates_folder+'IntroMisc.html'),
+             "*title*": "Movie Maker Documentation",
+             "*index_file*": io.folder_correct("./Documentation"),
+             "*how_it_works_file*":
+                 io.folder_correct("./Docs/Static/HowItWorks.html"),
+             "*how_to_edit_file*":
+                 io.folder_correct("./Docs/Static/HowToEdit.html"),
+             }
+
+# Handle the Sidebar
+SideBar(defaults, replacers, tables_folder)
+dstr = docstr.Docstr_Parsing('.')
+
+# Must do the TopNav bar first
+templateFilePathsOrder = ['TopNav']
+for i in template_filepaths:
+    if i not in templateFilePathsOrder:
+        templateFilePathsOrder.append(i)
+
+filesToWrite = {}
+for key in templateFilePathsOrder:
+    if 'table' in key:
+        for section in defaults.params:
+            filesToWrite[section] = TableHTMLFile(template_filepaths[key],
+                                                  defaults.params[section]
+                                                  )
+    else:
+        filesToWrite[key] = HTMLFile(template_filepaths[key],
+                                     replacers,
+                                     defaults)
+
+    if 'TopNav' in key:
+        replacers['*top_nav*'] = filesToWrite[key].fileTxt
--- docstring.py
+++ docstring.py
@@ -9,16 +9,28 @@ import os
 
 class Docstr_Parsing(object):
     """
-    Will parse the docstrings on top of the python files and also the README.md 
+    Will parse the docstrings on top of the python files and also the README.md
     files in each folder together to make the how_to_edit documentation.
+
+    Inputs:
+        * rootFolder => the folder to look for all the python files in
+
+    The docstrings are all stored in `self.docstr_dict' this is divded up into
+        directories and then files.
+
+    The docstring are parsed and written into a variable named `self.docstrTxt'
+        this is what should be inputted as the *Mov_Mak_Edit* var in the
+        How_to_edit.html file.
     """
-    
-    def __init__(self):
-        Docstr_Parsing.all_files = list(os.walk('.')) #save as list to use multiple times
+
+    def __init__(self, rootFolder):
+        # Save as list to use multiple times
+        Docstr_Parsing.all_files = list(os.walk(rootFolder))
         Docstr_Parsing._make_docstr_dict(self)
         Docstr_Parsing._get_README_files(self)
-        
-    
+
+        self.docstrTxt = "Still need to create the string. See docstring.py"
+
     def _get_docstr(self, fname):
         """
         Will get the docstring on the top of one of the python files.
@@ -32,23 +44,25 @@ class Docstr_Parsing(object):
                     delim = "'''"
                     break
             else:
-                print("no delim in %s\n\nskipping..."%fname)
+                print("no delim in %s\n\nskipping..." % fname)
                 return ""
             docstr = f.read().split(delim)[0]
         return docstr
-    
+
     @staticmethod
     def _get_README_files(self):
         """
         Will get all of the README files from each folder
         """
-        all_folders = [i for i in self.docstr_dict if os.path.isdir(i) and i != '.']
+        all_folders = [i for i in self.docstr_dict
+                       if os.path.isdir(i) and i != '.']
         for fold in all_folders:
             README_file = fold+'/README.md'
             if os.path.isfile(README_file):
                 with open(README_file, 'r') as f:
-                    self.docstr_dict[fold]['README'] = f.read().replace("\n", "")
-        
+                    self.docstr_dict[fold]['README'] = f.read().replace("\n",
+                                                                        "")
+
     @staticmethod
     def _make_docstr_dict(self):
         """
@@ -58,20 +72,8 @@ class Docstr_Parsing(object):
         for dpath, dnames, fnames in Docstr_Parsing.all_files:
             for fname in fnames:
                 if '.py' in fname and 'pyc' not in fname:
-                    fpath = dpath +'/' + fname
-                    docstr = self._get_docstr(fpath)   
-                    if dpath not in self.docstr_dict: self.docstr_dict[dpath] = {}
+                    fpath = dpath + '/' + fname
+                    docstr = self._get_docstr(fpath)
+                    if dpath not in self.docstr_dict:
+                        self.docstr_dict[dpath] = {}
                     self.docstr_dict[dpath][fpath] = docstr
-
-
-D = Docstr_Parsing()
-
-
-
-
-
-
-
-#how_to_edit_file = "./Templates/HTML/How_to_edit.html"
-#with open(how_to_edit_file, 'r') as f:
-#    txt = 
